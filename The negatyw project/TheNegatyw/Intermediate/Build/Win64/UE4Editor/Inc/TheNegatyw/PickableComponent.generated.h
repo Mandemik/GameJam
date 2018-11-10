02f6044be6a8a9aaadc461f33e744f3821b98716
Roll A ball <harewind@gmail.com>
2018-11-10 01:18:12 +0100

    Zmiana pracy kamery

---
+++ The negatyw project/TheNegatyw/Intermediate/Build/Win64/UE4Editor/Inc/TheNegatyw/PickableComponent.generated.h
@@ -0,0 +1,91 @@
+// Copyright 1998-2018 Epic Games, Inc. All Rights Reserved.
+/*===========================================================================
+	Generated code exported from UnrealHeaderTool.
+	DO NOT modify this manually! Edit the corresponding .h files instead!
+===========================================================================*/
+
+#include "UObject/ObjectMacros.h"
+#include "UObject/ScriptMacros.h"
+
+PRAGMA_DISABLE_DEPRECATION_WARNINGS
+#ifdef THENEGATYW_PickableComponent_generated_h
+#error "PickableComponent.generated.h already included, missing '#pragma once' in PickableComponent.h"
+#endif
+#define THENEGATYW_PickableComponent_generated_h
+
+#define TheNegatyw_Source_TheNegatyw_PickableComponent_h_14_RPC_WRAPPERS
+#define TheNegatyw_Source_TheNegatyw_PickableComponent_h_14_RPC_WRAPPERS_NO_PURE_DECLS
+#define TheNegatyw_Source_TheNegatyw_PickableComponent_h_14_INCLASS_NO_PURE_DECLS \
+private: \
+	static void StaticRegisterNativesUPickableComponent(); \
+	friend struct Z_Construct_UClass_UPickableComponent_Statics; \
+public: \
+	DECLARE_CLASS(UPickableComponent, UActorComponent, COMPILED_IN_FLAGS(0), CASTCLASS_None, TEXT("/Script/TheNegatyw"), NO_API) \
+	DECLARE_SERIALIZER(UPickableComponent)
+
+
+#define TheNegatyw_Source_TheNegatyw_PickableComponent_h_14_INCLASS \
+private: \
+	static void StaticRegisterNativesUPickableComponent(); \
+	friend struct Z_Construct_UClass_UPickableComponent_Statics; \
+public: \
+	DECLARE_CLASS(UPickableComponent, UActorComponent, COMPILED_IN_FLAGS(0), CASTCLASS_None, TEXT("/Script/TheNegatyw"), NO_API) \
+	DECLARE_SERIALIZER(UPickableComponent)
+
+
+#define TheNegatyw_Source_TheNegatyw_PickableComponent_h_14_STANDARD_CONSTRUCTORS \
+	/** Standard constructor, called after all reflected properties have been initialized */ \
+	NO_API UPickableComponent(const FObjectInitializer& ObjectInitializer); \
+	DEFINE_DEFAULT_OBJECT_INITIALIZER_CONSTRUCTOR_CALL(UPickableComponent) \
+	DECLARE_VTABLE_PTR_HELPER_CTOR(NO_API, UPickableComponent); \
+DEFINE_VTABLE_PTR_HELPER_CTOR_CALLER(UPickableComponent); \
+private: \
+	/** Private move- and copy-constructors, should never be used */ \
+	NO_API UPickableComponent(UPickableComponent&&); \
+	NO_API UPickableComponent(const UPickableComponent&); \
+public:
+
+
+#define TheNegatyw_Source_TheNegatyw_PickableComponent_h_14_ENHANCED_CONSTRUCTORS \
+private: \
+	/** Private move- and copy-constructors, should never be used */ \
+	NO_API UPickableComponent(UPickableComponent&&); \
+	NO_API UPickableComponent(const UPickableComponent&); \
+public: \
+	DECLARE_VTABLE_PTR_HELPER_CTOR(NO_API, UPickableComponent); \
+DEFINE_VTABLE_PTR_HELPER_CTOR_CALLER(UPickableComponent); \
+	DEFINE_DEFAULT_CONSTRUCTOR_CALL(UPickableComponent)
+
+
+#define TheNegatyw_Source_TheNegatyw_PickableComponent_h_14_PRIVATE_PROPERTY_OFFSET \
+	FORCEINLINE static uint32 __PPO__pickUpZone() { return STRUCT_OFFSET(UPickableComponent, pickUpZone); }
+
+
+#define TheNegatyw_Source_TheNegatyw_PickableComponent_h_11_PROLOG
+#define TheNegatyw_Source_TheNegatyw_PickableComponent_h_14_GENERATED_BODY_LEGACY \
+PRAGMA_DISABLE_DEPRECATION_WARNINGS \
+public: \
+	TheNegatyw_Source_TheNegatyw_PickableComponent_h_14_PRIVATE_PROPERTY_OFFSET \
+	TheNegatyw_Source_TheNegatyw_PickableComponent_h_14_RPC_WRAPPERS \
+	TheNegatyw_Source_TheNegatyw_PickableComponent_h_14_INCLASS \
+	TheNegatyw_Source_TheNegatyw_PickableComponent_h_14_STANDARD_CONSTRUCTORS \
+public: \
+PRAGMA_ENABLE_DEPRECATION_WARNINGS
+
+
+#define TheNegatyw_Source_TheNegatyw_PickableComponent_h_14_GENERATED_BODY \
+PRAGMA_DISABLE_DEPRECATION_WARNINGS \
+public: \
+	TheNegatyw_Source_TheNegatyw_PickableComponent_h_14_PRIVATE_PROPERTY_OFFSET \
+	TheNegatyw_Source_TheNegatyw_PickableComponent_h_14_RPC_WRAPPERS_NO_PURE_DECLS \
+	TheNegatyw_Source_TheNegatyw_PickableComponent_h_14_INCLASS_NO_PURE_DECLS \
+	TheNegatyw_Source_TheNegatyw_PickableComponent_h_14_ENHANCED_CONSTRUCTORS \
+private: \
+PRAGMA_ENABLE_DEPRECATION_WARNINGS
+
+
+#undef CURRENT_FILE_ID
+#define CURRENT_FILE_ID TheNegatyw_Source_TheNegatyw_PickableComponent_h
+
+
+PRAGMA_ENABLE_DEPRECATION_WARNINGS
